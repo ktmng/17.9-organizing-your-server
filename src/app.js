d2a6df591af0fa7dcd056521b887b047a35f2c8f
Kate Meng <katemeng08@gmail.com>
2020-10-08 10:24:15 -0700

    defined logger in app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require ('./logger')
 
 //1. require the bookmarks router:
 const bookmarksRouter = require('./bookmarks/bookmarks-router') 
